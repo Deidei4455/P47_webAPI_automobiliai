31b4c8351f0bc99886af53b57bd11c7edea12d08
Deidei4455 <ganuprauskas@gmail.com>
2025-01-29 11:38:34 +0200

    added cars_api_dict function

--- app01.py
+++ app01.py
@@ -15,7 +15,27 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
+@app.route("/api/cars")
+def cars_api_dict():
+    all_cars = Automobil.query.all()
+    duomenys_json = [
+        {
+            "id": car.id,
+            "make": car.make,
+            "model": car.model,
+            "color": car.color,
+            "year": car.year,
+            "price": car.price,
+            "fuel_type": car.fuel_type,
+            "date_created": car.date_created
+        }
+        for car in all_cars
+    ]
+    print(duomenys_json)
+    return jsonify(duomenys_json)
+
+
+@app.route("/api2/cars")
 def cars_api():
     all_cars = Automobil.query.all()
     cars_data = [CarsSchema.model_validate(car).model_dump() for car in all_cars]
@@ -24,7 +44,11 @@ def cars_api():
 
 @app.route("/frontend", methods=["GET", "POST"])
 def frontend():
-    all_cars = Automobil.query.all()
+    search_text = request.args.get("search")
+    if search_text:
+        all_cars = Automobil.query.filter(Automobil.make.ilike(f"%{search_text}%")).all()
+    else:
+        all_cars = Automobil.query.all()
     cars_data = [CarsSchema.model_validate(car).model_dump() for car in all_cars]
     return render_template("cars.html", cars=cars_data)
 
